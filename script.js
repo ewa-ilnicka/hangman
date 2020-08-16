4f557cba4d1f5845f5073dc330c4d92297dc1e7c
unknown <ewa.h.ilnicka@gmail.com>
2020-08-16 20:56:10 +0200

    Created list of letters in script

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 window.onload = function () {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const categories; // Array of topics
-    const chosenCategory; // Selected catagory
-    const word; // Selected word
-    const guess; // Geuss
-    const geusses = []; // Stored geusses
-    const lives; // Lives
-    const counter; // Count correct geusses
-    const space; // Number of spaces in word '-'
+    const categories;
+    let chosenCategory;
+    const word;
+    const guess;
+    let geusses = [];
+    const lives;
+    let counter;
+    const space;
+
+    let showLives = document.getElementById("mylives");
+    console.log(showLives);
+    let showCatagory = document.getElementById("scatagory");
+
+    let buttons = function () {
+        myButtons = document.getElementById("buttons");
+        letters = document.createElement("ul");
+
+        for (let i = 0; i < alphabet.length; i++) {
+            letters.id = "alphabet";
+            list = document.createElement("li");
+            list.id = "letter";
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    };
 
 }
