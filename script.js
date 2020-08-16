9f0f7aa2aed7634047aa65341e8e7a2d48b95e67
unknown <ewa.h.ilnicka@gmail.com>
2020-08-16 20:23:55 +0200

    Added variables

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.onload = function () {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const categories; // Array of topics
+    const chosenCategory; // Selected catagory
+    const word; // Selected word
+    const guess; // Geuss
+    const geusses = []; // Stored geusses
+    const lives; // Lives
+    const counter; // Count correct geusses
+    const space; // Number of spaces in word '-'
 
 }
