eda85b871c8476b3c36c60bb9e133650c9798961
unknown <ewa.h.ilnicka@gmail.com>
2020-08-17 10:22:20 +0200

    Play function

--- script.js
+++ script.js
@@ -157,4 +157,29 @@ window.onload = function () {
             }
         };
     };
+
+    play = function () {
+        categories = [
+            ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
+            ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+            ["manchester", "milan", "madrid", "amsterdam", "prague"]
+        ];
+
+        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+        word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+        word = word.replace(/\s/g, "-");
+        console.log(word);
+        buttons();
+
+        guesses = [];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        result();
+        comments();
+        selectCat();
+        canvas();
+    };
+
+    play();
 }
